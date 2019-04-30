cb5848f1187991e7e1c60b83ddc6f0af57ee6497
Chen Roth <rothchen@gmail.com>
2019-04-30 17:45:01 +0300

    refactor App to be a class

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,14 @@ interface IFolder {
     files: IFileProps[];
 }
 
-const App: React.FC = () => {
-    return (
-        <div className="App">
-            <File name="banana" extension="jpg" path="C:\banana.jpg"/>
-            <File name="README" extension="txt" path="C:\README.txt"/>
-            <File name="secret-code" extension="zip" path="C:\secret-code.zip"/>
-        </div>
-    );
+export class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <File name="banana" extension="jpg" path="C:\banana.jpg" />
+                <File name="README" extension="txt" path="C:\README.txt" />
+                <File name="secret-code" extension="zip" path="C:\secret-code.zip" />
+            </div>
+        );
+    }
 }
-
-export default App;
